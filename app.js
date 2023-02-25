b00a68dc57a2b30549f8220a846896e598b0811c
martin@mustbebuilt.co.uk <martinmustbebuilt@gmail.com>
2023-02-25 17:34:22 +0000

    New plan

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const path = require("path");
-const port = 3000;
+const port = process.env.PORT || 3000;
 const SessionDb = process.env.SESSION_STORAGE;
 // mongodb+srv://mjcAtlas01:<password>@mycluster01.ica5f.azure.mongodb.net/?retryWrites=true&w=majority
 const app = express();
